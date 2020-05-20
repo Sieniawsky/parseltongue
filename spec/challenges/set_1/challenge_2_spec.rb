c62bf70575de7fe065a1d4f43476ba55f162f404
Martin Sieniawski <m.sieniawski@live.ca>
2020-05-20 00:02:06 -0400

    Challenge spec

---
+++ spec/challenges/set_1/challenge_2_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'Set 1 challenge 2' do
+  describe 'Fixed XOR' do
+    let(:input_hex_1) { read_file('challenges/set_1/challenge_2/hex_1.txt') }
+    let(:input_hex_2) { read_file('challenges/set_1/challenge_2/hex_2.txt') }
+    let(:expected_hex) { read_file('challenges/set_1/challenge_2/expected_hex.txt') }
+
+    it 'performs xor on the 2 hex strings to produce the third' do
+      hex_1 = Parseltongue::ByteSequence.from_hex(input_hex_1)
+      hex_2 = Parseltongue::ByteSequence.from_hex(input_hex_2)
+      result = hex_1.xor(hex_2).to_hex
+
+      expect(result).to eq(expected_hex)
+    end
+  end
+end
